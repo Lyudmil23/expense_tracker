4639723325b3e7d5d72ab7bcbba1ca72b190f2f3
Lyudmil <lyudmil.lyubenov1@mail.bg>
2024-01-17 18:09:37 +0200

    Added files

--- expense_tracker/web_app/views.py
+++ expense_tracker/web_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from expense_tracker.web_app.forms import ExpenseForm
+from expense_tracker.web_app.models import Expense
+
 
 def index(request):
-    return render(request, 'web_app/index.html')
+    if request.method == 'POST':
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
+    expenses = Expense.objects.all()
+    expense_form = ExpenseForm()
+
+    context = {
+        'expense_form': expense_form,
+        'expenses': expenses,
+    }
+
+    return render(request, 'web_app/index.html', context)
